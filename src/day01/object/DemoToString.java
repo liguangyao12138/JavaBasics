5ce57d94e2ffea5135f3599b9d7993288fb184cf
lgy <583143445@qq.com>
2021-02-16 13:38:01 +0800

    笔记本测试

--- src/day01/object/DemoToString.java
+++ src/day01/object/DemoToString.java
@@ -3,7 +3,7 @@ package day01.object;
 /**
  * @author liguangyao
  * @date 2021/1/4 0:19
- * @description：
+ * @description： toString()使用
  */
 public class DemoToString {
     public static void main(String[] args) {
